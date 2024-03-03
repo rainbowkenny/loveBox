e6eded925a9dab78e577bd752ce370b78cd68529
S Hang <rainbowkenny@gmail.com>
2024-03-03 15:10:49 +0000

    feat: use gpt4

--- textGeneration.py
+++ textGeneration.py
@@ -1,16 +1,25 @@
 import config
-import openai
-openai.api_key=config.OPEN_AI_KEY
+from openai import OpenAI
+import re
 
+aikey=config.OPEN_AI_KEY
+client = OpenAI(api_key=aikey)
 def generate_love_sentence(prmpt):
-    # Use OpenAI's GPT-3 to generate a love sentence
-    # response = openai.ChatCompletion.create(
-    completion = openai.completions.create(
-      model="gpt-3.5-turbo-instruct",
-      # model="gpt-4-turbo-preview",
-      # model="gpt-4",
-      prompt=prmpt,
-      max_tokens=1000,
-      temperature=0.7
+    completion = client.chat.completions.create(
+      model="gpt-4-turbo-preview",
+      messages=[
+        {"role": "system", "content":prmpt},
+        {"role": "user", "content": "Tell me 5 jokes!"}
+      ],
+      n = 1,
+      temperature = 0.7
     )
-    return completion.choices[0].text.strip()
+    responses = completion.choices[0].message.content
+    strings = responses.split("\n")[1:-1]
+    filtered_array = [string for string in strings if string]
+    sentence = filtered_array[0]
+    result =re.sub(r'^\d*\.\s*', '',sentence)
+
+    return result 
+
+
