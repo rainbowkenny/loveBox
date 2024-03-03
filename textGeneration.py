4889ea12f46ee6e5b370482b0bdddec7cf2822b6
S Hang <rainbowkenny@gmail.com>
2024-03-03 15:33:10 +0000

    refactor: clean up response

--- textGeneration.py
+++ textGeneration.py
@@ -4,6 +4,13 @@ import re
 
 aikey=config.OPEN_AI_KEY
 client = OpenAI(api_key=aikey)
+def clean_responses(raw_responses):
+    strings = raw_responses.split("\n")[1:-1]
+    filtered_array = [string for string in strings if string]
+    for i in range(len(filtered_array)):
+        filtered_array[i] =re.sub(r'^\d*\.\s*', '',filtered_array[i])
+    return filtered_array
+    
 def generate_love_sentence(prmpt):
     completion = client.chat.completions.create(
       model="gpt-4-turbo-preview",
@@ -15,11 +22,7 @@ def generate_love_sentence(prmpt):
       temperature = 0.7
     )
     responses = completion.choices[0].message.content
-    strings = responses.split("\n")[1:-1]
-    filtered_array = [string for string in strings if string]
-    sentence = filtered_array[0]
-    result =re.sub(r'^\d*\.\s*', '',sentence)
-
+    result = clean_responses(responses)[0]
     return result 
 
 
